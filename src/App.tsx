bd7b9c2a22a28e47c9e3ebf735e4a09dbc1b0ca9
Vishal <vishal.rathore8oct@gmail.com>
2026-05-25 08:26:23 +0530

    feat: implement routing and create initial page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
-    <div className="text-3xl underline text-blue-500 cursor-pointer">App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </>
   );
 };
 

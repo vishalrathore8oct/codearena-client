5798406fb0f44c52a24f459abc37e65a99acd204
Vishal <vishal.rathore8oct@gmail.com>
2026-06-26 15:28:47 +0530

    feat: add loading state management for problem fetching operations into problems strore

--- src/store/useProblemStore.ts
+++ src/store/useProblemStore.ts
@@ -6,8 +6,12 @@ export const useProblemStore = create((set) => ({
   problems: [],
   problem: null,
   solvedProblems: [],
+  isAllProblemsLoading: false,
+  isProblemLoading: false,
+  isSolvedProblemsLoading: false,
 
   getAllProblems: async () => {
+    set({ isAllProblemsLoading: true });
     try {
       const res = await axiosInstance.get("/problems/get-all-problems");
       set({ problems: res.data.data.problems });
@@ -15,10 +19,13 @@ export const useProblemStore = create((set) => ({
     } catch (err: any) {
       toast.error(err.response?.data?.message || "Failed to fetch problems");
       throw err;
+    } finally {
+      set({ isAllProblemsLoading: false });
     }
   },
 
   getProblemById: async (id: string) => {
+    set({ isProblemLoading: true });
     try {
       const res = await axiosInstance.get(`/problems/get-problem/${id}`);
       set({ problem: res.data.data.problem });
@@ -26,10 +33,13 @@ export const useProblemStore = create((set) => ({
     } catch (err: any) {
       toast.error(err.response?.data?.message || "Error in getting problem");
       throw err;
+    } finally {
+      set({ isProblemLoading: false });
     }
   },
 
   getAllSolvedProblems: async () => {
+    set({ isSolvedProblemsLoading: true });
     try {
       const res = await axiosInstance.get("/problems/get-all-solved-problems");
       set({ solvedProblems: res.data.data.solvedProblems });
@@ -39,6 +49,8 @@ export const useProblemStore = create((set) => ({
         err.response?.data?.message || "Error getting solved problems",
       );
       throw err;
+    } finally {
+      set({ isSolvedProblemsLoading: false });
     }
   },
 }));

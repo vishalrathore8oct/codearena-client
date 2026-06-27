f419348741c27030d50a3ef7af3d3946552fcfad
Vishal <vishal.rathore8oct@gmail.com>
2026-06-27 07:07:11 +0530

    feat: add types authentication methods for email verification and password management in AuthState interface

--- src/types/authStore.ts
+++ src/types/authStore.ts
@@ -8,4 +8,8 @@ export interface AuthState {
   login: (data: LogInForm) => Promise<void>;
   signup: (data: SignUpForm) => Promise<void>;
   logout: () => Promise<void>;
+  verifyEmail: (token: string) => Promise<void>;
+  resendVerificationEmail: (email: string) => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
+  resetPassword: (token: string, password: string) => Promise<void>;
 }
